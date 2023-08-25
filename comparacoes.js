41d98e96b8cef102ad446fd96269bfa0c708871d
Andrei Barbassa <you@example.com>
2023-08-25 08:20:49 -0300

    atualziação teste 1

---
+++ comparacoes.js
@@ -0,0 +1,16 @@
+// == comparação implicita
+
+const numero = 5;
+const texto = '5';
+
+console.log(numero === texto);
+
+//typeof
+console.log(typeof numero);
+console.log(typeof texto);
+
+// == só compara o valor
+// === compara valor e tipo de dado (boas praticas)
+
+Number()
+String()
