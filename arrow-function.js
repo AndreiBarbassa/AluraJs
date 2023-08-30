92afe265023fd3169095e273d5c97f0a3669e815
Andrei Barbassa <you@example.com>
2023-08-30 15:27:55 -0300

    Arrow function e funcionamento do if

--- arrow-function.js
+++ arrow-function.js
@@ -12,13 +12,16 @@ const soma = (num1, num2) => num1 + num2
 //SE TIVER MAIS DE UMA LINHA É NECESSÁRIO USAR {} E RETURN:
 
 const somaNumerosPequenos = (num1, num2) => {
-    if (num1 || num2 > 10) { // || significa ou
+    if (num1 > 10 || num2 > 10) { // || significa ou
         return "Somente números de 1 a 9";
     } else {
-        num1 + num2;
+       return num1 + num2;
     }
 }
 
 // Hoisting: arrow function se comporta como expressão (nao da pra chamar antes de definir)
 
 //vantagem em orientação de objetos
+
+console.log(somaNumerosPequenos(4,99))
+console.log(somaNumerosPequenos(4,2))
---
+++ metodos-strings.js
@@ -0,0 +1,6 @@
+const palavra="alura";
+console.log(palavra.toUpperCase()) //ALURA
+console.log(palavra.toLowerCase()) //alura
+
+let novaString = "Programe nas principais linguagens e plataformas. Explore linguagens como ";
+console.log(novaString.concat("JavaScript,").concat(" Python,").concat(" e C#."))
